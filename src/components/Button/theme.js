e25e988b67429ff55782017eb0a4003e3a99035e
张晋佩 <302784355@qq.com>
2019-11-05 18:10:24 +0800

    init example

---
+++ src/components/Button/theme.js
@@ -0,0 +1,79 @@
+// 定义主色调
+const colorDefault = '#fff';
+const colorPrimary = '#317AF1';
+const colorInfo = '#66d4f8';
+const colorOrange = '#F8A441';
+const colorWarning = '#EBDE74';
+const colorSuccess = '#40db89';
+const colorError = '#ff6665';
+const colorDisabled = '#DCDFE6';
+const colorDark = '#313233';
+
+export default {
+  //Button - background color
+  btnDefaultColor: colorDefault,
+  btnGrayColor: colorDisabled,
+  btnPrimaryColor: colorPrimary,
+  btnInfoColor: colorInfo,
+  btnWarningColor: colorWarning,
+  btnOrangeColor: colorOrange,
+  btnSuccessColor: colorSuccess,
+  btnErrorColor: colorError,
+  btnDisabledColor: colorDisabled,
+
+  //Button - label color
+  btnLabelColorDefault: colorDark,
+  btnLabelColorPrimary: '#fff',
+  btnLabelColorInfo: '#fff',
+  btnLabelColorWarning: colorDark,
+  btnLabelColorOrange: '#fff',
+  btnLabelColorSuccess: '#fff',
+  btnLabelColorError: '#fff',
+  btnLabelColorDisabled: '#fff',
+  btnLabelColorGray: '#fff',
+  btnLabelLinkColor: colorPrimary,
+
+  //Button - font size
+  btnFontSizeXL: 36,
+  btnFontSizeLG: 36,
+  btnFontSizeMD: 28,
+  btnFontSizeSM: 24,
+  btnFontSizeXS: 18,
+
+  //Button - padding vertical
+  btnPaddingVerticalXL: 28,
+  btnPaddingVerticalLG: 18,
+  btnPaddingVerticalMD: 12,
+  btnPaddingVerticalSM: 8,
+  btnPaddingVerticalXS: 4,
+
+  //Button - padding horizontal
+  btnPaddingHorizontalXL: 35,
+  btnPaddingHorizontalLG: 25,
+  btnPaddingHorizontalMD: 15,
+  btnPaddingHorizontalSM: 10,
+  btnPaddingHorizontalXS: 5,
+
+  // loadingSize
+  btnLoadingSizeXL: 35,
+  btnLoadingSizeLG: 35,
+  btnLoadingSizeMD: 35,
+  btnLoadingSizeSM: 35,
+  btnLoadingSizeXS: 10,
+
+  //Button - border width
+  btnBorderWidth: 1,
+
+  //Button - border color
+  btnBorderColor: '#C9CDD5',
+
+  //Button - border radius
+  btnBorderRadiusXL: 6,
+  btnBorderRadiusLG: 6,
+  btnBorderRadiusMD: 4,
+  btnBorderRadiusSM: 3,
+  btnBorderRadiusXS: 3,
+  btnBorderRadiusRect: 0,
+  btnBorderRadiusRadius: 5,
+  btnBorderRadiusCircle: 1000,
+};
